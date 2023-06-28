dc4d9270935b76b18f85b6967c8beba4ee22e7f9
mapokapo <leopetrovic11@gmail.com>
2023-06-28 13:18:53 +0200

    Fixed images, re-enabled optimization flags

--- next.config.js
+++ next.config.js
@@ -3,15 +3,41 @@ const nextConfig = {
   experimental: {
     scrollRestoration: true,
   },
-  reactStrictMode: false,
-  productionBrowserSourceMaps: true,
-  swcMinify: false,
+  reactStrictMode: true,
+  productionBrowserSourceMaps: false,
+  swcMinify: true,
   images: {
     domains: [
       "github.com",
       "raw.githubusercontent.com",
       "storage.googleapis.com",
       "localhost",
+    ],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "github.com",
+        port: "",
+        pathname: "/**/*",
+      },
+      {
+        protocol: "https",
+        hostname: "raw.githubusercontent.com",
+        port: "",
+        pathname: "/**/*",
+      },
+      {
+        protocol: "https",
+        hostname: "storage.googleapis.com",
+        port: "",
+        pathname: "/**/*",
+      },
+      {
+        protocol: "https",
+        hostname: "localhost",
+        port: "3000",
+        pathname: "/**/*",
+      },
     ],
   },
 };
